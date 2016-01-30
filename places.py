4b2f09e9e7c8c41e4e89546d86032ccf7425b16b
Christoph Buelter <c.buelter@arcor.de>
2016-01-30 20:27:35 +0100

    Use logging and try to autodetect the .csv delimiter.

--- places.py
+++ places.py
@@ -5,6 +5,7 @@
 # http://stackoverflow.com/questions/22342097/is-it-possible-to-create-a-google-map-from-python
 
 import csv
+import logging
 import sys
 
 import pygeolib
@@ -40,6 +41,12 @@ PAGE_TEMPLATE = """
 </html>
 """
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S")
+log = logging.getLogger("places")
+
 
 class Map(object):
     """
@@ -55,6 +62,8 @@ class Map(object):
         self._points.append(coordinates)
 
     def __str__(self):
+        if not self._points:
+            return "No places to show. Check your .csv file."
         center_lat = sum((x[0] for x in self._points)) / len(self._points)
         center_long = sum((x[1] for x in self._points)) / len(self._points)
         markers_code = "\n".join(
@@ -68,7 +77,10 @@ class Map(object):
 def read_places_from_file(csvfile):
     """The .csv must have a single column with city names."""
     with open(csvfile) as f:
-        reader = csv.reader(f)
+        sniffer = csv.Sniffer()
+        dialect = sniffer.sniff(f.read())
+    with open(csvfile) as f:
+        reader = csv.reader(f, delimiter=dialect.delimiter)
         for row in reader:
             place_column = row[0]
             yield(place_column)
@@ -77,43 +89,46 @@ def read_places_from_file(csvfile):
 def get_geocoordinates_for_place(place):
     """Will return None if it can't resolve to coordinates."""
     try:
-        return Geocoder.geocode(str(place)).coordinates
+        coords = Geocoder.geocode(str(place)).coordinates
+        log.info("Found coordinates for: " + str(place))
+        return coords
     except pygeolib.GeocoderError:
-        print ("ERROR - Could not get coordinates for: " + str(place))
+        log.error("Could not get coordinates for: " + str(place))
         return None
 
 
 def visualize(csvfile):
     """Visualize places from .csv as google maps html page."""
 
-    print("INFO - Reading file...")
+    log.info("Reading: " + str(csvfile))
     places = read_places_from_file(csvfile)
     unique_places = list(set(list(places)))
     sorted_places = sorted(unique_places)
+    log.info("Read {0} unique places.".format(len(sorted_places)))
 
-    print("INFO - Querying coordinates...")
+    log.info("Querying coordinates.")
     points = []
     for place in sorted_places:
         point = get_geocoordinates_for_place(place)
         if point:
             points.append(point)
 
-    print("INFO - Assembling map...")
+    log.info("Assembling map.")
     custom_map = Map()
     for point in points:
         custom_map.add_point(point)
 
-    print("INFO - Writing results...")
+    log.info("Writing results.")
     with open(DEFAULT_OUTPUTFILE, "w") as out:
         out.write(str(custom_map))
 
-    print("INFO - Map written to " + DEFAULT_OUTPUTFILE)
-    print("INFO - Open it in a web browser to see the map.")
+    log.info("Map written to: " + DEFAULT_OUTPUTFILE)
+    log.info("Open it in a web browser to see the map.")
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) != 1:
-        print(USAGE)
+        log.info(USAGE)
     csvfile = args[0]
     visualize(csvfile)
